7963943c1d1d1290ea681705a54114cc95f2a250
Surjeet Singh <surjeet.devops@gmail.com>
2018-02-18 03:35:34 +0530

    adding terraform remote state backend as s3

--- terraform/tfplan.sh
+++ terraform/tfplan.sh
@@ -9,13 +9,6 @@ usage()
 
 AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials
 export AWS_SHARED_CREDENTIALS_FILE
-terraform init \
--backend=true \
--backend-config="bucket=terraform-demo-remote-state" \
--backend-config="key=dev/terraform.tfstate" \
--backend-config="region=us-east-1" \
--get=true \
--force-copy
 terraform get
 
 terraform plan -out ./plan.out -lock=false $@
