9445f5cee6628a05d92933f1ce0490353530c198
Surjeet Singh <surjeet.devops@gmail.com>
2018-02-18 03:02:26 +0530

    adding terraform remote state backend as s3

--- terraform/tfplan.sh
+++ terraform/tfplan.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+REGION = us-east-1
 usage()
 {
     echo 'To be called from the root/terraform-aws-vpc directory'
-    echo 'Usage : ./tplan'
+    echo 'Usage : ./tfplan'
     exit
 }
 
 AWS_SHARED_CREDENTIALS_FILE=~/.aws/credentials
 export AWS_SHARED_CREDENTIALS_FILE
-terraform init
+terraform init \
+-backend=true \
+-backend-config="bucket=terraform-demo-remote-state" \
+-backend-config="key=dev/terraform.tfstate" \
+-backend-config="region=$REGION" \
+-get=true \
+-force-copy
 terraform get
 
 terraform plan -out ./plan.out -lock=false $@
