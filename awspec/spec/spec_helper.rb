8437c384bc10e0e223c71fb9d38ec303d362bbd4
Harmeet Singh <harmeets@thoughtworks.com>
2018-02-18 19:27:55 +0530

    Adding Allure reporting for awspec tests, updating supported terraform version to cover 0.11.4

--- awspec/spec/spec_helper.rb
+++ awspec/spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'awspec'
+require 'allure-rspec'
+
 Awsecrets.load(secrets_path: File.expand_path('secrets.yml', File.dirname(__FILE__)))
+
+RSpec.configure do |c|
+    c.include AllureRSpec::Adaptor
+end
