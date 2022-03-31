874326e09b7c5065241d4515269db0d14386dcab
Surjeet Singh <surjeet.devops@gmail.com>
2022-03-31 20:32:37 +0100

    feat: refactor setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 #----Core Infrastructure file----#
 terraform {
   backend "s3" {
-    bucket = "terraform-demo-remote-state"
+    bucket = "terraform-cloudreach-remote-state"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
@@ -12,45 +12,51 @@ data "aws_caller_identity" "current" {}
 module "vpc" {
   source = "./modules/vpc"
 
-  region              = "${var.region}"
-  product             = "${var.product}"
-  cidr_block          = "${var.cidr_block}"
-  environment         = "${var.environment}"
-  availability_zones  = "${var.availability_zones}"
-  public_subnet_cidr  = "${var.public_cidr_block}"
-  private_subnet_cidr = "${var.private_cidr_block}"
+  region              = var.region
+  product             = var.product
+  cidr_block          = var.cidr_block
+  environment         = var.environment
+  availability_zones  = var.availability_zones
+  public_subnet_cidr  = var.public_cidr_block
+  private_subnet_cidr = var.private_cidr_block
+  data_subnet_cidr    = var.data_cidr_block
+  default_outbound_acl_rules = concat(local.network_acls["default_outbound"])
+  private_inbound_acl_rules  = concat(local.network_acls["private_inbound"])
+  public_inbound_acl_rules   = concat(local.network_acls["public_inbound"])
+  data_inbound_acl_rules     = concat(local.network_acls["data_inbound"])
+
 }
 
-module "iam" {
-  source = "./modules/iam"
-}
-
-
-module "bastion" {
-  source = "./modules/bastion"
-
-  product                      = "${var.product}"
-  region                       = "${var.region}"
-  environment                  = "${var.environment}"
-  vpc_id                       = "${module.vpc.vpc_id}"
-  ami_bastion                  = "${var.ami_bastion}"
-  instance_type_bastion        = "${var.instance_type_bastion}"
-  public_subnet_id             = "${module.vpc.vpc_public_subnet}"
-  key_name                     = "${var.key_name}"
-  iam_instance_profile_bastion = "${module.iam.iam_instance_profile_bastion}"
-}
-
-module "serviceOne" {
-  source = "./modules/serviceOne"
-
-  product                         = "${var.product}"
-  region                          = "${var.region}"
-  environment                     = "${var.environment}"
-  vpc_id                          = "${module.vpc.vpc_id}"
-  ami_serviceOne                  = "${var.ami_serviceOne}"
-  instance_type_serviceOne        = "${var.instance_type_serviceOne}"
-  private_subnet_id               = "${module.vpc.vpc_private_subnet}"
-  key_name                        = "${var.key_name}"
-  iam_instance_profile_serviceOne = "${module.iam.iam_instance_profile_serviceOne}"
-  bastion-sg                      = "${module.bastion.bastion-sg-id}"
-}
+//module "iam" {
+//  source = "./modules/iam"
+//}
+//
+//
+//module "bastion" {
+//  source = "./modules/bastion"
+//
+//  product                      = "${var.product}"
+//  region                       = "${var.region}"
+//  environment                  = "${var.environment}"
+//  vpc_id                       = "${module.vpc.vpc_id}"
+//  ami_bastion                  = "${var.ami_bastion}"
+//  instance_type_bastion        = "${var.instance_type_bastion}"
+//  public_subnet_id             = "${module.vpc.vpc_public_subnet}"
+//  key_name                     = "${var.key_name}"
+//  iam_instance_profile_bastion = "${module.iam.iam_instance_profile_bastion}"
+//}
+//
+//module "serviceOne" {
+//  source = "./modules/serviceOne"
+//
+//  product                         = "${var.product}"
+//  region                          = "${var.region}"
+//  environment                     = "${var.environment}"
+//  vpc_id                          = "${module.vpc.vpc_id}"
+//  ami_serviceOne                  = "${var.ami_serviceOne}"
+//  instance_type_serviceOne        = "${var.instance_type_serviceOne}"
+//  private_subnet_id               = "${module.vpc.vpc_private_subnet}"
+//  key_name                        = "${var.key_name}"
+//  iam_instance_profile_serviceOne = "${module.iam.iam_instance_profile_serviceOne}"
+//  bastion-sg                      = "${module.bastion.bastion-sg-id}"
+//}
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -12,6 +12,10 @@ output "vpc_private_subnet" {
   value = "${aws_subnet.private.*.id}"
 }
 
+output "vpc_data_subnet" {
+  value = "${aws_subnet.data.*.id}"
+}
+
 output "vpc_public_routetable" {
   value = "${aws_route_table.public.id}"
 }
@@ -19,3 +23,4 @@ output "vpc_public_routetable" {
 output "vpc_private_routetable" {
   value = "${aws_route_table.private.*.id}"
 }
+
